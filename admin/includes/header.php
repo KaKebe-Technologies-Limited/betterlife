10cc5a905d5b818a47f96f612bd632e12a823adf
Kakebe Technologies Limited <kakebetech.comms@gmail.com>
2026-08-25 13:51:32 +0300

    cote

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -58,7 +58,7 @@ try {
   <aside class="sidebar" id="adminSidebar">
     <div class="brand">
       <img src="<?= asset_url($logo) ?>" alt="Logo">
-      <div><strong></strong><span>Content Dashboard</span></div>
+      <div><strong></strong><span></span></div>
     </div>
     <nav>
       <?php foreach ($navItems as $group): ?>
